1038d85948c0d6030ab23f021dbc805963d0e8a9
smrutidash07 <136309187+smrutidash07@users.noreply.github.com>
2023-06-12 13:21:38 +0530

    Update steamlit_app.py
    Update Breakfast menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new healthy Diner')
 
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega3 and Blueberry oatmeanl')
-streamlit.title('kale, spinach and rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry oatmeanl')
+streamlit.text('kale, spinach and rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
