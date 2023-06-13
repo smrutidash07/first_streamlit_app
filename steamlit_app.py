435aab5e7836111f13a899fb35f354ddcf5d46c8
smrutidash07 <136309187+smrutidash07@users.noreply.github.com>
2023-06-13 18:05:36 +0530

    Update steamlit_app.py
    indentation

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,16 +42,15 @@ try:
       streamlit.dataframe(back_from_function)
       
  #except URLError as e:
-   
-   streamlit.header("this fruit load list contains:")
-   #Snowflake related functions
-   def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
+streamlit.header("this fruit load list contains:")
+#Snowflake related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+   my_cur.execute("select * from fruit_load_list")
+   return my_cur.fetchall()
       
-   #Add a buttonto load a fruit
-   if streamlit.button('Get fruit load list"):
-                       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-                       my_data_rows=get_fruit_load_list()
-                       streamlit.dataframe(my_data_rows)
+#Add a buttonto load a fruit
+if streamlit.button('Get fruit load list"):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
