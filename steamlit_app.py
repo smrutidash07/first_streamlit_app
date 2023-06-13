d7829357708a02ac82a39559131d0ab8b6b8e296
smrutidash07 <136309187+smrutidash07@users.noreply.github.com>
2023-06-13 16:40:14 +0530

    Update steamlit_app.py
    updated funtion

--- steamlit_app.py
+++ steamlit_app.py
@@ -26,10 +26,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block( called a funtion)
-def get_fruityvice_data(this_fruit_choice)
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   streamlit.dataframe(fruityvice_normalized)
+   return fruityvice_normalized
    
 #New section to display fruityvice response data
 streamlit.header("Fruityvice Fruit Advice!")
